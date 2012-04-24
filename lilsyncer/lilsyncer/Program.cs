fe0e4b1df21a65fe5680d764373dc3d9c8dd9077
Tiernan OToole <tierno@lotas-smartman.net>
2012-04-24 20:41:10 +0200

    adding initial support for Google Drive...

--- lilsyncer/lilsyncer/Program.cs
+++ lilsyncer/lilsyncer/Program.cs
@@ -12,7 +12,7 @@ namespace lilsyncer
         static string photoStreamFolder = @"C:\Users\<yourname>\Pictures\Photo Stream\My Photo Stream";
         static string photoStreamUploadFolder = @"C:\Users\<yourname>\Pictures\Photo Stream\Uploads";
         static string skyDriveFolder = @"C:\Users\<yourname>\SkyDrive\SkyDrive camera roll";
-
+        static string gDriveFolder = @"C:\Users\<yourname>\Google Drive\Photos";
         
 
         static void Main(string[] args)
@@ -20,6 +20,7 @@ namespace lilsyncer
             FileSystemWatcher dropboxWatcher = new FileSystemWatcher(dropboxFolder);
             FileSystemWatcher photoStreamWatcher = new FileSystemWatcher(photoStreamFolder);
             FileSystemWatcher skydriveWatcher = new FileSystemWatcher(skyDriveFolder);
+            FileSystemWatcher googleDriveWatcher = new FileSystemWatcher(gDriveFolder);
 
 
             
@@ -29,6 +30,9 @@ namespace lilsyncer
             photoStreamWatcher.EnableRaisingEvents = true;
             skydriveWatcher.Created += new FileSystemEventHandler(skydriveWatcher_Created);
             skydriveWatcher.EnableRaisingEvents = true;
+            googleDriveWatcher.Created += new FileSystemEventHander(googleDriveWatcher_Created);
+            googleDriveeWatcher.EnableRaisingEvents = true;
+            
             Console.WriteLine("Watching folders... ");
             Console.WriteLine("Hit enter to kill");
             Console.ReadLine();
@@ -40,6 +44,7 @@ namespace lilsyncer
             Console.WriteLine("Found {0} on Skydrive", e.Name);
             CopyToPhotoStream(e);
             CopyToDropbox(e);
+            CopyToGDrive(e);
         }
 
         static void photoStreamWatcher_Created(object sender, FileSystemEventArgs e)
@@ -47,6 +52,7 @@ namespace lilsyncer
             Console.WriteLine("Found {0} on PhotoStream", e.Name);
             CopyToDropbox(e);
             CopyToSkyDrive(e);
+            CopyToGDrive(e);
         }
 
         static void dropboxWatcher_Created(object sender, FileSystemEventArgs e)
@@ -54,6 +60,14 @@ namespace lilsyncer
             Console.WriteLine("Found {0} on Dropbox", e.Name);
             CopyToPhotoStream(e);
             CopyToSkyDrive(e);
+            CopyToGDrive(e);
+        }
+        
+        static void googleDriveWatcher_Created(object sender, FileSystemEvenArgs e){
+            Console.WriteLine("Found {0} on Google Drive", e.Name);
+            CopyToPhotoStream(e);
+            CopyToSkyDrive(e);
+            CopyToDropbox(e);
         }
 
         private static void CopyToDropbox(FileSystemEventArgs e)
@@ -92,6 +106,16 @@ namespace lilsyncer
             else
             {
                 Console.WriteLine("Not copying {0}. Already exists on Skydrive", e.Name);
+            }
+        }
+        
+        private static void CopyToGDrive(FileSystemEventArgs e){
+            if(!File.Exists(Path.Combine(gDriveFolder, e.Name))){
+                Console.WriteLine("Copying {0} to GDrive", e.Name);
+                File.Copy(e.FullPath, Path.Combine(gDriveFolder, e.Name));
+            }
+            else{
+                Console.Writeline("Not copying {0}. Already exists on GDrive", e.Name);
             }
         }
 
